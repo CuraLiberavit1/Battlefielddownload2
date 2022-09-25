d0092505b65b315581efc7a90e2bba9c01a9bc11
CuraLiberavit1 <112109836+CuraLiberavit1@users.noreply.github.com>
2022-09-25 20:39:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html>
+<html>
 
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewpost" content="width=device-width, initial-scale=1">
+<title> Contact Us </title>
+
+
+
+
+
+
+
+
+</head>
+
+<body>
+<div class="contact">
+<form method="POST>
+<h2> Contact Us</h2>
+
+<input type="text" placeholder="Name" name="user_name" required>
+
+<input type="email" placeholder="Email" name="user_email" required>
+
+<input type="text" placeholder="Subject" name="user_subject" required>
+
+<textarea placeholder="Type message" rows="6" name="user_msg" required></textarea>
+<button name="contact_btn">Send Message</button>
+</form>
+</div>
+</body>
+</html>
